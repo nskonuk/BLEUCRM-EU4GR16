ee24a9c744f50b96cf7fc7c6d90f1d3030c2b06d
Rustam Jafarov <j.rustam@hotmail.com>
2021-02-17 22:42:01 +0100

    LogOut Functionality added

--- src/test/java/com/bleucrm/runners/CukesRunner.java
+++ src/test/java/com/bleucrm/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/bleucrm/step_definitions",
         dryRun = false,
-        tags = "@login"
+        tags = "@logout"
 )
 public class CukesRunner {
 }
