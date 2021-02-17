20b6846c237f2e0200cc9f97df3b72ff1b7a8bb5
nrslc <cankarnursel@gmail.com>
2021-02-17 11:01:18 +0100

    Login Page is created

---
+++ src/test/java/com/bleucrm/pages/LoginPage.java
@@ -0,0 +1,31 @@
+package com.bleucrm.pages;
+
+import com.bleucrm.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(name = "USER_LOGIN")
+    public WebElement userName;
+
+    @FindBy(name = "USER_PASSWORD")
+    public WebElement password;
+
+    @FindBy(className = "login-btn")
+    public WebElement submit;
+
+    public void login(String userNameStr, String passwordStr) {
+        userName.sendKeys(userNameStr);
+        password.sendKeys(passwordStr);
+        submit.click();
+
+    }
+
+
+}
